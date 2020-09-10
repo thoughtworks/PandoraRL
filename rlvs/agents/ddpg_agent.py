Updated DDPG with corrected gradient correction

rlvs/agents/ddpg_agent.py
@@ -1,4 +1,5 @@
-# refered from https://github.com/sol0invictus/RL_Codes/blob/master/DDPG/v1/ddpg.py
+# refered from https://github.com/germain-hug/Deep-RL-Keras
+
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Sequential, Model
@@ -10,129 +11,138 @@ from .network import Actor, Critic
 from .noise import OrnsteinUhlenbeckActionNoise
 
 class DDPGAgent:
-    MAX_EPSILON = 1
-    MIN_EPSILON = 0.01
-    ACTOR_LEARNING_RATE = 1e-3,
-    CRITIQ_LEARNING_RATE = 1e-3,
-    EPSILON_MIN_ITER = 5000
-    DELAY_TRAINING = 300
-    GAMMA = 0.95
-    BATCH_SIZE = 32
-    TAU = 0.08
-    RANDOM_REWARD_STD = 1.0
-    NOISE_SCALE = 0.1
+    ACTOR_LEARNING_RATE  = 0.00005
+    CRITIQ_LEARNING_RATE = 0.00005
+    TAU                  = 0.001
+
+    DELAY_TRAINING       = 300
+    GAMMA                = 0.99
+
+    BATCH_SIZE           = 32
+    BUFFER_SIZE          = 20000
+
+    RANDOM_REWARD_STD    = 1.0
+    NOISE_SCALE          = 0.001
+    
     
     def __init__(self, env):
-        '''
-Create a DDPG agent
-Parameters: 
-action_bounds (array): array with min and max action values, with shape (2, number of output values)
-        example: 
-        np.array([
-          [x_min, y_min, theta_min], 
-          [x_max, y_max, theta_max]
-        ])
-        
-        '''
         self.input_shape = env.input_shape
-        self.multi_output_count = env.action_space.n_outputs
+        self.action_shape = env.action_space.n_outputs
         self.eps = 0.9
         self.action_bounds = env.action_space.action_bounds
-        self.memory = Memory(500000)
+        self.memory = Memory(self.BUFFER_SIZE)
         self.env = env
-        self.exploration_noise = OrnsteinUhlenbeckActionNoise(mu=np.zeros(env.action_space.n_outputs))
+        self.exploration_noise = OrnsteinUhlenbeckActionNoise(size=self.env.action_space.n_outputs)
         self.noise_scaling = self.NOISE_SCALE * (self.action_bounds[1] - self.action_bounds[0])
 
-        self._actor = Actor(self.input_shape, self.multi_output_count, 1e-3)
-        self._critiq = Critic(self.input_shape, self.multi_output_count, 1e-3)
+        self._actor = Actor(
+            self.input_shape,
+            self.action_shape,
+            self.ACTOR_LEARNING_RATE,
+            self.TAU
+        )
 
-    def get_action(self, state, noise_scale):
+        self._critiq = Critic(
+            self.input_shape,
+            self.action_shape,
+            self.CRITIQ_LEARNING_RATE,
+            self.TAU
+        )
+
+    def get_action(self, state, step=None):
+        # Try Brownian Motion as a candidate for stochastic noise, currently using Ornstein–Uhlenbeck process
         # Explore AdaptiveParamNoiseSpec, with normalized action space
         # https://github.com/l5shi/Multi-DDPG-with-parameter-noise/blob/master/Multi_DDPG_with_parameter_noise.ipynb
         action = self.action_bounds[1] * self._actor.predict(state).flatten()
-        action += self.exploration_noise() * self.noise_scaling
-        #import random
 
-        #return [random.uniform(*bounds) for bounds in self.action_bounds.transpose()]
-        return np.clip(action, *self.action_bounds)
-    
+        if step is not None:
+            action += self.exploration_noise.generate(step)
+            
+        x, y, t = np.clip(action, *self.action_bounds)
+        return np.array([np.round(x), np.round(y), t])
+
     def play(self, num_train_episodes):
-        returns = []
-        q_losses = []
-        mu_losses = []
-        num_steps = 0
-        action_noise=0.99
-        max_episode_length=500
-        decay = 0.99
-        noise_ep = 10
+        returns      = []
+        q_losses     = []
+        mu_losses    = []
+        num_steps    = 0
+        action_noise = 0.99
+        max_episode_length = 500
+        decay        = 0.99
+        noise_ep     = 10
         r = False
 
         for i_episode in range(num_train_episodes):
             state_t, episode_return, episode_length, terminal = self.env.reset(), 0, 0, False
-            self.exploration_noise.reset()
-            print("Actual states: ", [self.env.block.shift_x, self.env.block.shift_y], [self.env.block.block_x, self.env.block.block_y, self.env.block.rotate_angle])
+            self.exploration_noise = OrnsteinUhlenbeckActionNoise(size=self.env.action_space.n_outputs)
             while not (terminal or (episode_length == max_episode_length)):
                 # For the first `start_steps` steps, use randomly sampled actions
                 # in order to encourage exploration.
-                
-                if i_episode < noise_ep:
-                  action = self.get_action(state_t, action_noise)
-                  r = True
-                else:
-                  action = self.env.action_space.sample()
-                  r = False
-                
-                # Keep track of the number of steps done
-                num_steps += 1                
-                # Step the env
+                action = self.get_action(state_t, episode_length)
                 state_t_1, reward, terminal = self.env.step(action)
-                episode_return += reward
-                episode_length += 1
-                
-                # Ignore the "done" signal if it comes from hitting the time
-                # horizon (that is, when it's an artificial terminal signal
-                # that isn't based on the agent's state)
                 d_store = False if episode_length == max_episode_length else terminal
                 
-                # Store experience to replay buffer
-                self.memory.add_sample([state_t[0], action, reward, state_t_1[0], d_store])
+                self.memorize(state_t[0], action, reward, state_t_1[0], d_store)
+
+                num_steps += 1                
                 
-                # Assign next state to be the current state on the next round
+                episode_return += reward
+                episode_length += 1
                 state_t = state_t_1
-                # returns.append([self.env.block.sandbox, self.env.block.original_sandbox, reward])
-
-                print(r, action, reward, episode_length, [self.env.block.block_x, self.env.block.block_y, self.env.block.rotate_angle])
                 
-            self.update_network(mu_losses, q_losses)
-            decay *= decay
-            action_noise *= 0.7
-            print("Episode:", i_episode + 1, "Return:", episode_return, 'episode_length:', episode_length)
+                print(r, action, reward, episode_length, [self.env.block.block_x, self.env.block.block_y, self.env.block.rotate_angle, self.env.block.shift_x, self.env.block.shift_y])
+                self.update_network()
+                
+            mean, stdev = self.gather_stats()
+            returns.append([i_episode + 1, episode_length, mean, stdev])
 
-        return (returns,q_losses,mu_losses)
+            print("Episode:", i_episode + 1, "Return:", episode_return, 'episode_length:', episode_length, 'stats (m, s)', [mean, stdev])
 
+        return returns
 
-    def update_network(self, mu_losses, q_losses):
+    def memorize(self, state, action, reward, next_state, done):
+        self.memory.add_sample({
+            'state': state,
+            'action': action,
+            'reward': reward,
+            'next_state': next_state,
+            'done': done
+        })
+        
+    def update_network(self):
         batch = self.memory.sample(self.BATCH_SIZE)
         batch_len = len(batch)
         
-        states = np.array([val[0] for val in batch])
-        actions = np.array([val[1] for val in batch])
-        rewards = np.array([val[2] for val in batch])
-        next_states = np.array([val[3] for val in batch ])
-        terminals = np.array([val[4] for val in batch ])
-
-        state_tensor=tf.convert_to_tensor(states)
+        states = np.array([val['state'] for val in batch])
+        actions = np.array([val['action'] for val in batch])
+        rewards = np.array([val['reward'] for val in batch])
+        next_states = np.array([val['next_state'] for val in batch ])
+        terminals = np.array([val['done'] for val in batch ])
 
         target_actions = self._actor.predict_target(next_states)
-        target_q = self._critiq.predict_target(next_states, target_actions)
-            
-        train_q = (rewards + (1 - terminals) * self.GAMMA * target_q.reshape((batch_len,))).reshape((batch_len,1))
-        self._critiq.fit(states, actions, train_q)
-        predicted_actions = self._actor.tensors(states)
+        target_q = self._critiq.predict_target([next_states, target_actions])
+        
+        critic_target = (
+            rewards + (1 - terminals) * self.GAMMA * target_q.reshape((batch_len,))
+        ).reshape((batch_len,1))
+        
+        self._critiq.train_on_batch(states, actions, critic_target)
+        predicted_actions = self._actor.predict(states)
 
-        action_grads, state_grads = self._critiq.action_gradients(state_tensor, predicted_actions)
-        self._actor.fit(states, action_grads)
+        action_grads = self._critiq.action_gradients(states, predicted_actions)
+        self._actor.train(states, actions, action_grads[0])
 
         self._actor.update_target_network()
         self._critiq.update_target_network()
         
+    def gather_stats(self):
+        score = []
+        for k in range(10):
+            old_state = self.env.reset()
+            cumul_r, done = 0, False
+            while not done:
+                a = self.get_action(old_state)
+                old_state, r, done = self.env.step(a)
+                cumul_r += r
+                score.append(cumul_r)
+        return np.mean(np.array(score)), np.std(np.array(score))

rlvs/agents/network.py
@@ -1,30 +1,45 @@
 import tensorflow as tf
-from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense, GlobalMaxPooling2D, Convolution2D, Conv2D, MaxPooling2D, Concatenate, Input
-from tensorflow.keras.optimizers import Adam
+from keras.models import Sequential, Model
+from keras.layers import Activation, Dropout, Flatten, Dense, GlobalMaxPooling2D, Convolution2D, Conv2D, MaxPooling2D, Concatenate, Input, GaussianNoise
+from keras.initializers import RandomUniform
+from keras.optimizers import Adam
+import keras.backend as keras_backend
 import numpy as np
 
-
 class Critic:
-    def __init__(self, input_shape, action_shape, learning_rate):
-        self.critic = self._create_network(input_shape, action_shape, learning_rate)
-        self.critic_target = self._create_network(input_shape, action_shape, learning_rate)
+    def __init__(self, input_shape, action_shape, learning_rate, tau=0.001):
+        self._tau = tau
+        self._learning_rate = learning_rate
+        self._state_shape = input_shape
+        self._action_shape = action_shape
+        
+        self.critic, self._input_layer, self._action_layer = self._create_network()
+        self.critic_target, _, __ = self._create_network()
 
-    def predict(self, states, actions):
-        return self.critic.predict((actions, states))
+        self._action_gradients = keras_backend.function(
+            [self.critic.input[0], self.critic.input[1]], keras_backend.gradients(
+                self.critic.output, [self.critic.input[1]]
+            )
+        )
 
-    def predict_target(self, states, actions):
-        return self.critic_target.predict((actions, states))
+    def predict_target(self, inp):
+        return self.critic_target.predict(inp)
+    
+    def update_target_network(self):
+        model_weights = np.array(self.critic.get_weights())
+        target_weights = np.array(self.critic_target.get_weights())
+        target_weights = self._tau * model_weights + (1 - self._tau) * target_weights 
+        self.critic_target.set_weights(target_weights)
 
-    def fit(self, states, actions, q_values):
-        return self.critic.fit((actions, states), q_values, epochs = 20, batch_size = 1)
+    def train_on_batch(self, states, actions, critic_target):
+        return self.critic.train_on_batch([states, actions], critic_target)
+
 
     def action_gradients(self, states, actions):
-        return tf.gradients(self.critic((actions, states)),  (actions, states))
+        return self._action_gradients([states, actions])
 
-
-    def _create_network(self, input_shape, action_shape, learning_rate):
-        conv_model = Input(shape=input_shape)
+    def _create_network(self):
+        conv_model = Input(shape=self._state_shape)
 
         conv_model_1 = Conv2D(64, 5, 5,  activation='relu')(conv_model)
         conv_model_1 = Conv2D(64, 4, 4,  activation='relu')(conv_model)
@@ -34,57 +49,54 @@ class Critic:
         conv_model_1 = Dense(256, activation='relu')(conv_model_1)
         conv_model_1 = Model(inputs=conv_model, outputs=conv_model_1)
 
-        action_model = Input(shape=[action_shape])
-        action_model_1 = Dense(256, activation='relu')(action_model)
+        action_model = Input(shape=[self._action_shape])
+        action_model_1 = Dense(256, activation='linear')(action_model)
         action_model_1 = Model(inputs=action_model, outputs=action_model_1)
 
-        intermediate = Concatenate()([action_model_1.output, conv_model_1.output])
+        intermediate = Concatenate()([conv_model_1.output, action_model_1.output])
         intermediate = Dense(256, activation='relu')(intermediate)
         intermediate = Dropout(0.5)(intermediate)
 
-        value_layer = Dense(1, activation='linear')(intermediate)
+        value_layer = Dense(1, activation='linear', kernel_initializer=RandomUniform())(intermediate)
 
-        model = Model([action_model, conv_model], value_layer)
+        model = Model([conv_model, action_model], value_layer)
+        model.compile(optimizer=Adam(lr=self._learning_rate), loss='mse')
 
-        model.compile(optimizer=Adam(lr=learning_rate), 
-              loss='binary_crossentropy',
-              metrics=['accuracy']
-        )
-
-        return model
-
-    def update_target_network(self):
-        decay=0.99
-        temp1 = np.array(self.critic_target.get_weights())
-        temp2 = np.array(self.critic.get_weights())
-        
-        temp3 = decay * temp1 + (1 - decay) * temp2
-        self.critic_target.set_weights(temp3)
-
+        return model, conv_model, action_model
 
 
 class Actor:
-    def __init__(self, input_shape, action_shape, learning_rate):                
-        self.action_grads = None
+    def __init__(self, input_shape, action_shape, learning_rate, tau=0.001):
+        self._learning_rate = learning_rate
+        self._tau = tau
+        self._state_shape = input_shape
+        self._action_shape = action_shape
         
-        self.actor = self._create_network(input_shape, action_shape, learning_rate)
-        self.actor_target = self._create_network(input_shape, action_shape, learning_rate)
+        self.actor, self._input_layer = self._create_network()
+        self.actor_target, _ = self._create_network()
+        self.optimizer = self._create_optimizer()
 
+    def train(self, states, actions, action_gradients):
+        self.optimizer([states, action_gradients])
+        
     def predict(self, input_state):
         return self.actor.predict(input_state)
-
-    def tensors(self, input_state):
-        return self.actor(input_state)
-
-    def fit(self, states, actions):
-        return self.actor.fit(states, actions, epochs = 20, batch_size = 1, steps_per_epoch=1)
 
     def predict_target(self, input_state):
         return self.actor_target.predict(input_state)
 
+    def _create_optimizer(self):
+        action_gdts = keras_backend.placeholder(shape=(None, self._action_shape))
+        params_grad = tf.gradients(self.actor.output, self.actor.trainable_weights, -action_gdts)
+        grads = zip(params_grad, self.actor.trainable_weights)
+        return keras_backend.function(
+            inputs=[self.actor.input, action_gdts],
+            outputs=[keras_backend.constant(1)],
+            updates=[tf.train.AdamOptimizer(self._learning_rate).apply_gradients(grads)]
+        )
 
-    def _create_network(self, input_shape, action_shape, learning_rate):
-        conv_model = Input(shape=input_shape)
+    def _create_network(self):
+        conv_model = Input(shape=self._state_shape)
 
         conv_model_1 = Conv2D(64, 5, 5,  activation='relu')(conv_model)
         conv_model_1 = Conv2D(64, 4, 4,  activation='relu')(conv_model)
@@ -93,22 +105,20 @@ class Actor:
         conv_model_1 = Flatten()(conv_model_1)
         conv_model_1 = Dense(256, activation='relu')(conv_model_1)
         conv_model_1 = Dense(128, activation='relu')(conv_model_1)
+        conv_model_1 = GaussianNoise(1.0)(conv_model_1)
 
-        action_layer = Dense(action_shape, activation='linear')(conv_model_1)
+        action_layer = Dense(
+            self._action_shape,
+            activation='tanh',
+            kernel_initializer=RandomUniform()
+        )(conv_model_1)
 
         model = Model(conv_model, action_layer)
-
-        model.compile(optimizer=Adam(lr=learning_rate), 
-              loss='binary_crossentropy',
-              metrics=['accuracy']
-        )
-
-        return model        
+        return model, conv_model
 
     def update_target_network(self):
-        decay=0.99
-        temp1 = np.array(self.actor_target.get_weights())
-        temp2 = np.array(self.actor.get_weights())
+        actor_weights = np.array(self.actor_target.get_weights())
+        target_weights = np.array(self.actor.get_weights())
         
-        temp3 = decay * temp1 + (1 - decay) * temp2
-        self.actor_target.set_weights(temp3)
+        target_weights = self._tau * actor_weights + (1 - self._tau) * target_weights
+        self.actor_target.set_weights(target_weights)

rlvs/agents/noise.py
@@ -1,28 +1,19 @@
-'''
-## Ornstein-Uhlenbeck (OU) Noise ##
-# Taken from: https://github.com/openai/baselines/blob/master/baselines/ddpg/noise.py
-# Creates OU noise process to add temporally-correlated noise to the action space during training for exploration purposes.
-'''
-
 import numpy as np
-
-class OrnsteinUhlenbeckActionNoise:
-    def __init__(self, mu, sigma=0.3, theta=0.15, dt=1e-2, x0=None):
+class OrnsteinUhlenbeckActionNoise(object):
+    """ Ornstein-Uhlenbeck Noise (original code by @slowbull)
+    """
+    def __init__(self, theta=0.15, mu=0, sigma=1, x0=0, dt=1e-2, n_steps_annealing=100, size=1):
         self.theta = theta
-        self.mu = mu
         self.sigma = sigma
-        self.dt = dt
+        self.n_steps_annealing = n_steps_annealing
+        self.sigma_step = - self.sigma / float(self.n_steps_annealing)
         self.x0 = x0
-        self.reset()
+        self.mu = mu
+        self.dt = dt
+        self.size = size
 
-    def __call__(self):
-        x = self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt + \
-                self.sigma * np.sqrt(self.dt) * np.random.normal(size=self.mu.shape)
-        self.x_prev = x
+    def generate(self, step):
+        sigma = max(0, self.sigma_step * step + self.sigma)
+        x = self.x0 + self.theta * (self.mu - self.x0) * self.dt + sigma * np.sqrt(self.dt) * np.random.normal(size=self.size)
+        self.x0 = x
         return x
-
-    def reset(self):
-        self.x_prev = self.x0 if self.x0 is not None else np.zeros_like(self.mu)
-
-    def __repr__(self):
-        return 'OrnsteinUhlenbeckActionNoise(mu={}, sigma={})'.format(self.mu, self.sigma)

rlvs/block_world/block.py
@@ -58,8 +58,8 @@ class Block:
 
     def __init__(self, width, height, mask):
 
-        self.sandbox_width = 5 * width
-        self.sandbox_height = 5 * height
+        self.sandbox_width = 6 * width
+        self.sandbox_height = 6 * height
         self.surface_width = 2 * width
         self.surface_height = 2 * height
 
@@ -77,22 +77,32 @@ class Block:
         self.surface_index = 1
         self.shift_x = randint(0, max_x_shift) + (self.surface_index * self.surface_width)
         self.shift_y = randint(0, max_y_shift) + (self.surface_index * self.surface_height)
-
-        self.block_x, self.block_y = [
-            [1,1],
-            [1, self.sandbox_height - self._block.shape[1]],
-            [self.sandbox_width - self._block.shape[0], 1],
-            [self.sandbox_width - self._block.shape[0], self.sandbox_height - self._block.shape[1]]
-        ][randint(0,3)]
-
+        self._max_dist = self._max_distance()
+        self.block_x, self.block_y = randint(1, self.sandbox_width - self._block.shape[0]), randint(1, self.sandbox_height - self._block.shape[1])
 
         self.update_sandbox()
         self.original_sandbox = np.copy(self.sandbox)
 
+    
+    def _max_distance(self):
+        bounds = np.array([
+            [0, 0, -180],
+            [0, self.sandbox_height, -180 ],
+            [self.sandbox_width, 0, -180],
+            [self.sandbox_width, self.sandbox_height, -180],
+            [0, 0, 180],
+            [0, self.sandbox_height, 180 ],
+            [self.sandbox_width, 0, 180],
+            [self.sandbox_width, self.sandbox_height, 180]
+        ])
+
+        return max(self.distance(x, y, t) for x,y,t in bounds)
+        
+    
     @property
     def action_bounds(self):
-        #return [[-self._block.shape[0], -self._block.shape[1], -180], [self._block.shape[0], self._block.shape[1], 180]]
-        return [[-(self.sandbox_width - self._block.shape[0]), -(self.sandbox_height - self._block.shape[1]), -180], [self.sandbox_width - self._block.shape[0], self.sandbox_height - self._block.shape[1], 180]]
+        return [[-2*self._block.shape[0], -2*self._block.shape[1], -10], [2*self._block.shape[0], 2*self._block.shape[1], 10]]
+        #return [[-(self.sandbox_width - self._block.shape[0]), -(self.sandbox_height - self._block.shape[1]), -180], [self.sandbox_width - self._block.shape[0], self.sandbox_height - self._block.shape[1], 180]]
         
     @property
     def block(self):
@@ -129,28 +139,40 @@ class Block:
         ] += self.rotate_block(-self.rotate_angle + rotate_angle)
 
         self.rotate_angle -= rotate_angle
+        if self.rotate_angle < -180:
+           self.rotate_angle += 360
+
+        if self.rotate_angle > 180:
+           self.rotate_angle -= 360
+           
         self.block_x = new_x
         self.block_y = new_y
         
-    
-    def rotate_block(self, angle):
-        return ndimage.rotate(self._block, angle=angle, mode='nearest',  reshape=False)
-
-    def score(self):
-        surface_bonus = 0
-        surface_x = range(self.surface_index * self.surface_width, ( self.surface_index + 1 ) * self.surface_width)
-        surface_y = range(self.surface_index * self.surface_height, ( self.surface_index + 1 ) * self.surface_height)
-        if round(self.block_x) in surface_x and round(self.block_y) in surface_y:
-            surface_bonus = 100
-            
+    def distance(self, x, y, theta):
         pi_2 = np.deg2rad(90)
-        r = np.sqrt(self.block_x**2 + self.block_y**2)
-        phi =  pi_2 if self.block_x == 0 else np.arctan(self.block_y/self.block_x)
-        theta = phi + np.deg2rad(self.rotate_angle)
+        r = np.sqrt(x**2 + y**2)
+        phi =  pi_2 if x == 0 else np.arctan(y/x)
+        theta = phi + np.deg2rad(theta)
 
         r_actual = np.sqrt(self.shift_x**2 + self.shift_y**2)
         phi_actual = pi_2 if self.shift_x == 0 else np.arctan(self.shift_y/self.shift_x)
         theta_actual = phi_actual
 
-        dist = np.sqrt(r**2 + r_actual**2 - 2*(r * r_actual * np.cos(theta - theta_actual)))
-        return self.max_score if dist < 0.01 else (1/dist) + surface_bonus
+        return np.sqrt(r**2 + r_actual**2 - 2*(r * r_actual * np.cos(theta - theta_actual)))
+        
+    def rotate_block(self, angle):
+        return ndimage.rotate(self._block, angle=angle, mode='nearest',  reshape=False)
+
+    @property
+    def perfect_fit(self):
+        return 0 == self.distance(self.block_x, self.block_y, self.rotate_angle)
+    
+    def score(self):
+        surface_bonus = 0
+        surface_x = range(self.surface_index * self.surface_width, ( self.surface_index + 1 ) * self.surface_width)
+        surface_y = range(self.surface_index * self.surface_height, ( self.surface_index + 1 ) * self.surface_height)
+        if round(self.block_x) in surface_x and round(self.block_y) in surface_y:
+            surface_bonus = 0.1
+            
+        dist = self.distance(self.block_x, self.block_y, self.rotate_angle)
+        return 1 - (dist/self._max_dist)**0.4 + 0.1

rlvs/block_world/env.py
@@ -28,11 +28,11 @@ class Env:
         try:
             self.block.update_sandbox(*action)
         except:
-            reward = -0.1
+            reward = -0.0001
             self.block.update_sandbox()
         else: 
             reward = self.block.score()
             
         state = np.expand_dims(self.block.sandbox.reshape(self.input_shape), axis=0)
-        terminal = reward in [self.block.max_score]
+        terminal = (reward == -0.0001 or self.block.perfect_fit)
         return state.astype(dtype='float32'), reward, terminal