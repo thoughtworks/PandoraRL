19bc2ccf25f298d36178849772f3ecb4c53dd30d
Justin <justinj@thoughtworks.com>
2020-09-16 16:33:53 +0530

    Adding PDBQT dataset

--- rlvs/molecule_world/datasets.py
+++ rlvs/molecule_world/datasets.py
@@ -36,7 +36,27 @@ class PafnucyData(Data):
                 )
             ) for complex in self.complexes_path
         ]
-        
+
+class PDBQTData(Data):
+    DATA_PATH=f'{ROOT_PATH}/pdbqt_data'
+
+    def __init__(self):
+        super(DudeProteaseData, self).__init__()
+        self.complexes_path = [
+            ('6Y2F_MOD.pdbqt', 'a-ketoamide-13b.pdbqt')
+        ]
+
+        self._complexes = [
+            lambda: (
+                OB_to_mol(
+                    read_to_OB(filename=f'{self.DATA_PATH}/{complex}', filetype="pdbqt"),
+                    mol_type=-1
+                ), OB_to_mol(
+                    read_to_OB(filename=f'{self.DATA_PATH}/{ligand}', filetype="pdbqt"),
+                    mol_type=1
+                )
+            ) for complex, ligand in self.complexes_path
+        ]
 
 class DudeProteaseData(Data):
     DATA_PATH=f'{ROOT_PATH}/dude_protease'
@@ -66,7 +86,7 @@ class DataStore:
 
     @classmethod
     def init(cls):
-        cls.DATA_STORES = [PafnucyData(), DudeProteaseData()]
+        cls.DATA_STORES = [PDBQTData(), PafnucyData(), DudeProteaseData()]
 
     @classmethod
     def next(cls):
