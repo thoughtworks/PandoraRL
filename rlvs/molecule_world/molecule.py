2a61708d3c77c929f38e747a284747d459070335
Kritika Gupta <kritikagupta@Kritikas-MacBook-Pro.local>
2020-09-30 13:00:01 +0530

    Making molecule object compatible with graph conv model

--- rlvs/molecule_world/molecule.py
+++ rlvs/molecule_world/molecule.py
@@ -22,26 +22,28 @@ class Molecule(ConvMol):
         self.T = lambda x,y,z : np.array([[1,0,0,x], [0,1,0,y], [0,0,1,z], [0,0,0,1]]).astype(float)
         self.rmsd = RMSD(self)
 
+    def get_coords(self):
+        return self.atom_features[:, 0:3]
+
+    def set_coords(self, new_coords):
+        assert new_coords.shape==(self.n_atoms, 3)
+        self.atom_features[:, 0:3] = new_coords
+
     def randomize(self, box_size):
         x, y_ = np.random.uniform(0, box_size/4, (2,))
         self.update_pose(x, y_, 0, 0,0,0)
         
     def atom_range(self, axis):
-        values = self.coords[:,axis]
+        values = self.get_coords()[:,axis]
         minimum, maximum = min(values), max(values)
         return minimum, maximum, abs(maximum-minimum)
-                          
-    # def shift_origin(self, new_origin):
-    #     self.origin = np.array(new_origin, copy=True).astype(float).reshape(1,3)
-    #     self.coords -= self.origin
-    #     assert(self.coords.shape == (self.num_atoms, 3))
         
     def get_centroid(self):
-        return self.coords.mean(axis=0)
+        return self.get_coords().mean(axis=0)
     
     def homogeneous(self):
         ''' returns homogeneous form of coordinates (column major form) for translation and rotation matrix multiplication '''
-        return np.concatenate((self.coords, np.ones((self.coords.shape[0], 1))), axis = 1).T
+        return np.concatenate((self.get_coords(), np.ones((self.get_coords().shape[0], 1))), axis = 1).T
 
     def update_pose(self, x, y, z, roll, pitch, yaw):
         self.translate(x, y, z)
@@ -49,7 +51,7 @@ class Molecule(ConvMol):
         
     def translate(self,x,y,z):
         ''' translate along axes '''
-        self.coords = (self.T(x,y,z)@self.homogeneous()).T[:, 0:3]
+        self.set_coords((self.T(x,y,z)@self.homogeneous()).T[:, 0:3])
         
     def rotate(self, axis_seq, angles, degrees):
         '''
@@ -61,9 +63,9 @@ class Molecule(ConvMol):
         R = (Rotation.from_euler(seq=axis_seq, angles=angles, degrees=degrees)).as_matrix()
         R = np.concatenate((np.concatenate((R, np.array([0,0,0]).reshape(3, -1)), axis = 1), np.array([0,0,0,1]).reshape(-1, 4)), axis = 0)
 
-        self.coords = (self.T(x,y,z)@(R@(self.T(-x,-y,-z)@self.homogeneous()))).T[:,0:3]
+        self.set_coords((self.T(x,y,z)@(R@(self.T(-x,-y,-z)@self.homogeneous()))).T[:,0:3])
 
-        assert(self.coords.shape == (self.num_atoms, 3))
+        assert(self.get_coords().shape == (self.n_atoms, 3))
         
     def crop(self, center_coord, x, y, z):
         check_range = lambda x, min_x, max_x: True if (x >= min_x and x < max_x) else False
@@ -72,37 +74,18 @@ class Molecule(ConvMol):
         min_y, max_y = center_coord[:,1][0] - y, center_coord[:,1][0] + y
         min_z, max_z = center_coord[:,2][0] - z, center_coord[:,2][0] + z
         
-        mask = np.zeros(self.num_atoms, dtype=bool)
-        for i, (X,Y,Z) in enumerate(self.coords):
+        mask = np.zeros(self.n_atoms, dtype=bool)
+        for i, (X,Y,Z) in enumerate(self.get_coords()):
             mask[i] = check_range(X, min_x, max_x) and check_range(Y, min_y, max_y) and check_range(Z, min_z, max_z)
             
         self.apply_crop_mask(mask)
 
     def apply_crop_mask(self, mask):
-        self.coords = self.coords[mask, :]
-        self.features = self.features[mask, :]
-        self.num_atoms = self.features.shape[0]
-        
-        
-    def set_in_grid(self, grid):
-        grid_coords = grid.convert_to_grid_coords(self.coords)
-        for (x,y,z), f in zip(grid_coords, self.features):
-            grid.tensor4D[x,y,z] += f
-            
-    def plot(self, grid, s=20, alpha=1, color='r'):
-        grid_coords = grid.convert_to_grid_coords(self.coords)
-        origin = grid.convert_to_grid_coords(self.origin)
-        X, Y, Z = [], [], []
-        for (x,y,z) in grid_coords:
-            X.append(x)
-            Y.append(y)
-            Z.append(z)
-        grid.ax.scatter(X,Y,Z, s=s, alpha=alpha, color=color)
-        grid.ax.scatter([origin[0,0]], [origin[0,1]], [origin[0,2]], s=s, alpha=1, color=color, marker = "+")
-        #print(f"Plotting X={X}, Y={Y}, Z={Z}")
+        self.atom_features = self.atom_features[mask, :]
+        self.n_atoms = self.atom_features.shape[0]
     
     def summary(self):
-        print(f"Number of heavy atoms in molecule = {self.num_atoms}")
-        print(f"Number of features = {self.num_features}")
+        print(f"Number of heavy atoms in molecule = {self.n_atoms}")
+        print(f"Number of features = {self.n_feat}")
         print(f"Max distance between atoms along\nx:{self.atom_range(0)}\ny:{self.atom_range(1)}\nz:{self.atom_range(2)}")
         
