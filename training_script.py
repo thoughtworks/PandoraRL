e7163f20ee24da27a067cb1e1edb8945616724b2
Justin <justinj@thoughtworks.com>
2020-10-30 15:31:56 +0530

    Updated training to 50k

--- training_script.py
+++ training_script.py
@@ -7,5 +7,5 @@ run_id = 0
 folder = "./Results/"
 path_prefix = f"{folder}run{run_id}_"
 agent = DDPGAgentGNN(env, weights_path=path_prefix+"weights_intermediate", log_filename=path_prefix+"training_log.log")
-actions =  agent.play(5000)
+actions =  agent.play(50000)
 agent.save_weights(path_prefix+"weights_final")
