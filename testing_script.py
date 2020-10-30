a99a6d22940757e4ccb7601306802ea46b7f1054
Kritika Gupta <kritikagupta@Kritikas-MacBook-Pro.local>
2020-10-30 14:35:37 +0530

    Updated testing script

--- testing_script.py
+++ testing_script.py
@@ -1,11 +1,19 @@
 from rlvs.molecule_world.env import TestGraphEnv
 from rlvs.agents.ddpg_agent import DDPGAgentGNN
 import joblib
+import os
 
 from src.main.Path import Path
 
 protein_input = Path.PROTEIN_FILE_PATH
+protein_filetype = os.path.splitext(protein_input)[1][1:]
+
+# ligand file input
 ligand_input = Path.LIGAND_FILE_PATH
+ligand_filetype = os.path.splitext(ligand_input)[1][1:]
+# ligand string input
+ligand_input = ""
+ligand_filetype = "smiles_string"
 
 # specify model path
 actor_weights = "./Results/run5_actor.h5"
@@ -20,7 +28,14 @@ joblib.dump(DataStore.scaler, scaler_filename)
 
 scaler = joblib.load(scaler_filename) 
 max_steps = 10
-env = TestGraphEnv(scaler=scaler, protein_path=protein_input, ligand_path=ligand_input, protein_filetype="pdbqt", ligand_filetype="pdbqt")
+env = TestGraphEnv(
+    scaler=scaler, 
+    protein_path=protein_input, 
+    ligand_path=ligand_input, 
+    protein_filetype=protein_filetype, 
+    ligand_filetype=ligand_filetype,
+    prepare=False
+)
 agent = DDPGAgentGNN(env, weights_path = "", log_filename=Path.TESTING_LOG_FILE_PATH)
 agent.test(max_steps=max_steps, path_actor_weights=actor_weights, path_critic_weights=critic_weights)
 
