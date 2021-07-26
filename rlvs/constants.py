6dd2bd18fcaed2b299cd79b9c08985ead2e569ab
Justin <justinj@thoughtworks.com>
2021-07-26 17:33:02 +0530

    Add constant module

---
+++ rlvs/constants.py
@@ -0,0 +1,15 @@
+class ComplexConstants:
+    DISTANCE_THRESHOLD = 75
+    GOOD_FIT = 0.006
+
+
+class AgentConstants:
+    ACTOR_LEARNING_RATE = 0.00005
+    CRITIQ_LEARNING_RATE = 0.0001
+    TAU = 0.001
+
+    GAMMA = 0.99
+
+    BATCH_SIZE = 32
+    BUFFER_SIZE = 200000
+    EXPLORATION_EPISODES = 10000
