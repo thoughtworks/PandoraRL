4d52244814519c8e2966de9b7b271b9a976bba36
Justin Jose <justinj@e4rhome>
2021-08-12 23:21:38 +0530

    Update Atom to include edge distance, VDWr and smart params, residue

--- rlvs/molecule_world/atom.py
+++ rlvs/molecule_world/atom.py
@@ -9,21 +9,24 @@ from .featurizer import Featurizer
 class Atoms:
     def __init__(self, molecule_type, obmol, pdb_structure=None):
         self._atoms = []
-        self.featurizer = Featurizer(obmol)
+        featurizer = Featurizer(obmol)
         if molecule_type == MoleculeType.PROTEIN:
             pdb_atoms = [atom for atom in pdb_structure.get_atoms()]
             self._atoms = [
                 Atom(
-                    ob_atom.GetIndex(),
+                    (idx:=ob_atom.GetIndex()),
                     molecule_type,
-                    pdb_atoms[ob_atom.GetIndex()].name,
-                    ob_atom.GetAtomicNum(),
-                    pdb_atoms[ob_atom.GetIndex()].coord,
+                    pdb_atoms[idx].name,
+                    (atm_no:=ob_atom.GetAtomicNum()),
+                    pdb_atoms[idx].coord,
                     ob_atom.GetHyb(),
                     ob_atom.GetHvyDegree(),
                     ob_atom.GetHeteroDegree(),
                     ob_atom.GetPartialCharge(),
-                    pdb_atoms[ob_atom.GetIndex()]
+                    ob.GetVdwRad(atm_no),
+                    featurizer.smarts_patterns[idx],
+                    pdb_atoms[idx],
+                    ob_atom.GetResidue().GetName()
                 )
                 for ob_atom in ob.OBMolAtomIter(obmol)
             ]
@@ -31,29 +34,37 @@ class Atoms:
         elif molecule_type == MoleculeType.LIGAND:
             self._atoms = [
                 Atom(
-                    atom.GetIndex(),
+                    (idx:=atom.GetIndex()),
                     molecule_type,
-                    self.featurizer.atom_codes[atom.GetAtomicNum()],
-                    atom.GetAtomicNum(),
-                    [atom.GetX(), atom.GetY(), atom.GetZ()],
+                    featurizer.atom_codes[(atm_no:=atom.GetAtomicNum())],
+                    atm_no,
+                    np.array([atom.GetX(), atom.GetY(), atom.GetZ()]),
                     atom.GetHyb(),
                     atom.GetHvyDegree(),
                     atom.GetHeteroDegree(),
-                    atom.GetPartialCharge()
+                    atom.GetPartialCharge(),
+                    ob.GetVdwRad(atm_no),
+                    featurizer.smarts_patterns[idx]
                 )
                 for atom in ob.OBMolAtomIter(obmol)
             ]
 
         self.bonds = [
-            Bond(bond.GetBeginAtom().GetIndex(), bond.GetEndAtom().GetIndex())
+            Bond(
+                self._atoms[bond.GetBeginAtom().GetIndex()],
+                self._atoms[bond.GetEndAtom().GetIndex()],
+                bond.GetLength()
+            )
             for bond in ob.OBMolBondIter(obmol)
         ]
 
         self.features = torch.tensor([
-            atom.features(self.featurizer) for atom in self._atoms
+            atom.features(featurizer) for atom in self._atoms
         ], dtype=torch.float)
 
-        self.edge_index = torch.vstack([bond.edge for bond in self.bonds]).t().contiguous()
+        self.edge_index = torch.vstack(
+            [bond.edge for bond in self.bonds]
+        ).t().contiguous()
 
     @property
     def x(self):
@@ -73,17 +84,22 @@ class Atoms:
     def __len__(self):
         return len(self._atoms)
 
+    def __iter__(self):
+        return self._atoms.__iter__()
+
     def where(self, condition):
         return [atom for atom in self._atoms if condition(atom)]
 
 
 class Atom:
     def __init__(
-            self, idx, molecule_type,
+            self, idx=-1, molecule_type=MoleculeType.LIGAND,
             name=None, atomic_num=None,
             coord=None, hyb=None, hvy_degree=None,
             hetro_degree=None, partial_charge=None,
-            pdb_atom=None
+            VDWr=None, smarts=None, pdb_atom=None,
+            residue=None
+            
     ):
         self.idx = idx
         self._type = molecule_type
@@ -95,6 +111,17 @@ class Atom:
         self.hvy_degree = hvy_degree
         self.hetro_degree = hetro_degree
         self.partial_charge = partial_charge
+        self.VDWr = VDWr
+        self.hydrophobic,\
+            self.aromatic,\
+            self.acceptor,\
+            self.donor,\
+            self.ring = np.array(
+            smarts, dtype=bool
+        )
+
+        self.residue = residue
+        self.bonds = np.array([])
 
     @property
     def molecule_type(self):
@@ -115,20 +142,24 @@ class Atom:
     def features(self, featurizer):
         return featurizer.featurize(self)
 
+    def add_bond(self, bond):
+        self.bonds = np.append(self.bonds, bond)
+
 
 class Bond:
-    def __init__(self, atom_a, atom_b):
+    def __init__(self, atom_a, atom_b, bond_length, update_edge=True):
         self.atom_a = atom_a
         self.atom_b = atom_b
+        self.distance = np.sqrt(np.sum((atom_a.coord - atom_b.coord) ** 2))
+        self.lenght = bond_length
+
+        if update_edge:
+            atom_a.add_bond(self)
+            atom_b.add_bond(self)
 
     @property
     def edge(self):
-        return torch.tensor([[self.atom_a, self.atom_b], [self.atom_b, self.atom_a]], dtype=torch.long)
-
-# Element type
-#  name
-#  element
-#  position
-#  get_features: similar to featurizer object
-#  c_alpha
-#  heavy_atom
+        return torch.tensor([
+            [self.atom_a.idx, self.atom_b.idx],
+            [self.atom_b.idx, self.atom_a.idx]
+        ], dtype=torch.long)
