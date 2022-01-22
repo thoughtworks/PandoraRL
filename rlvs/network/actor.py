0ec042a7e1cc5abe58bc62b7f1c86f9dd5e4b740
Justin Jose <justinj@thoughtworks.com>
2022-01-22 07:33:17 +0530

    [Justin] Fix linter warnings

--- rlvs/network/actor.py
+++ rlvs/network/actor.py
@@ -1,23 +1,27 @@
 import torch
 import torch.nn as nn
-from torch.nn import Linear, LayerNorm, ReLU, GRU
-from torch_geometric.data import Data
 import torch.nn.functional as F
 import torch_geometric.transforms as T
 import numpy as np
-from ..agents.utils import timeit
 
-from torch_geometric.nn import GENConv, DeepGCNLayer, global_mean_pool as gap, global_max_pool as gmp
-import torch_geometric.nn as t_nn
+from torch_geometric.nn import GENConv, DeepGCNLayer, \
+    global_mean_pool as gap, global_max_pool as gmp
+
 
 def fanin_init(size, fanin=None):
     fanin = fanin or size[0]
     v = 1. / np.sqrt(fanin)
     return torch.Tensor(size).uniform_(-v, v)
 
+
 transform = T.Cartesian(cat=False)
+
+
 class ActorGNN(nn.Module):
-    def __init__(self, input_shape, edge_shape, action_shape, learning_rate, tau=0.001, init_w=3e-3):
+    def __init__(
+            self, input_shape, edge_shape,
+            action_shape, learning_rate, tau=0.001, init_w=3e-3
+    ):
         super(ActorGNN, self).__init__()
         self._learning_rate = learning_rate
         self._tau = tau
@@ -26,13 +30,13 @@ class ActorGNN(nn.Module):
 
         self.node_encoder = nn.Linear(input_shape, hidden_channels)
         self.edge_encoder = nn.Linear(edge_shape, hidden_channels)
-        
+
         self.layers = torch.nn.ModuleList()
         for i in range(1, num_layers + 1):
             conv = GENConv(hidden_channels, hidden_channels, aggr='softmax',
                            t=1.0, learn_t=True, num_layers=2, norm='layer')
-            norm = LayerNorm(hidden_channels, elementwise_affine=True)
-            act = ReLU(inplace=True)
+            norm = nn.LayerNorm(hidden_channels, elementwise_affine=True)
+            act = nn.ReLU(inplace=True)
 
             layer = DeepGCNLayer(conv, norm, act, block='res+', dropout=0.1,
                                  ckpt_grad=i % 3)
@@ -44,7 +48,7 @@ class ActorGNN(nn.Module):
     def init_weights(self, init_w):
         self.node_encoder.weight.data = fanin_init(self.node_encoder.weight.data.size())
         self.edge_encoder.weight.data = fanin_init(self.edge_encoder.weight.data.size())
-        
+
         self.action_layer_in.weight.data = fanin_init(self.action_layer_in.weight.data.size())
         self.action_layer_out.weight.data.uniform_(-init_w, init_w)
 
@@ -52,20 +56,25 @@ class ActorGNN(nn.Module):
     def forward(self, complex_):
         complex_data, complex_edge_index, \
             complex_edge_attr, complex_batch = complex_.x, complex_.edge_index,\
-                complex_.edge_attr, complex_.batch
+            complex_.edge_attr, complex_.batch
 
         complex_data = self.node_encoder(complex_data)
         complex_edge_attr = self.edge_encoder(complex_edge_attr)
 
-        complex_data = F.relu(self.complex_gcn_in1(complex_data, complex_edge_index, complex_edge_attr))
-        complex_data, complex_edge_index, complex_edge_attr, complex_batch,  perm, score  = self.pool1(complex_data, complex_edge_index, complex_edge_attr, complex_batch)
-        complex_data = F.relu(self.complex_gcn_in2(complex_data, complex_edge_index, complex_edge_attr))
+        complex_data = F.relu(
+            self.complex_gcn_in1(complex_data, complex_edge_index, complex_edge_attr)
+        )
+        complex_data, complex_edge_index, complex_edge_attr, \
+            complex_batch,  perm, score = self.pool1(
+                complex_data, complex_edge_index, complex_edge_attr, complex_batch
+            )
+        complex_data = F.relu(
+            self.complex_gcn_in2(complex_data, complex_edge_index, complex_edge_attr)
+        )
 
-        
         # complex_data = self.complex_gcn_in(complex_data, complex_edge_index, complex_edge_attr)
 
-        molecule_data = global_mean_pool(complex_data, complex_batch)
-        # import pdb;pdb.set_trace()
+        molecule_data = gap(complex_data, complex_batch)
         action = F.relu(self.action_layer_in(molecule_data))
         action = self.action_layer_out(action)
         action = torch.tanh(action)
@@ -73,7 +82,11 @@ class ActorGNN(nn.Module):
 
 
 class ActorDQN(ActorGNN):
-    def __init__(self, input_shape, edge_shape, action_shape, learning_rate, tau=0.001, init_w=3e-3):
+    def __init__(
+            self, input_shape, edge_shape,
+            action_shape, learning_rate,
+            tau=0.001, init_w=3e-3
+    ):
         super(
             ActorDQN, self
         ).__init__(
@@ -83,7 +96,7 @@ class ActorDQN(ActorGNN):
     def forward(self, complex_):
         complex_data, complex_edge_index, \
             complex_edge_attr, complex_batch = complex_.x, complex_.edge_index,\
-                complex_.edge_attr, complex_.batch
+            complex_.edge_attr, complex_.batch
         complex_data = self.node_encoder(complex_data)
         complex_edge_attr = self.edge_encoder(complex_edge_attr)
 
@@ -94,19 +107,26 @@ class ActorDQN(ActorGNN):
 
         complex_data = self.layers[0].act(self.layers[0].norm(complex_data))
         complex_data = F.dropout(complex_data, p=0.1, training=self.training)
-        molecule_data = torch.cat([gmp(complex_data, complex_batch), gap(complex_data, complex_batch)], dim=1)
+        molecule_data = torch.cat([
+            gmp(complex_data, complex_batch),
+            gap(complex_data, complex_batch)
+        ], dim=1)
         action = F.relu(self.action_layer_in(molecule_data))
         action = self.action_layer_out(action)
         return action
 
+
 class ActorGCN(ActorDQN):
-    def __init__(self, input_shape, edge_shape, action_shape, learning_rate, tau=0.001, init_w=3e-3):
+    def __init__(
+            self, input_shape, edge_shape,
+            action_shape, learning_rate, tau=0.001, init_w=3e-3
+    ):
         super(
             ActorDQN, self
         ).__init__(
             input_shape, edge_shape, action_shape, learning_rate, tau, init_w
         )
-    
+
     def forward(self, complex_):
         action = super().forward(complex_)
         return F.softmax(action)
--- rlvs/network/critic.py
+++ rlvs/network/critic.py
@@ -2,9 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-from ..agents.utils import timeit, USE_CUDA
 
 from torch_geometric.nn import GENConv, global_mean_pool
+
 
 def fanin_init(size, fanin=None):
     fanin = fanin or size[0]
@@ -13,7 +13,10 @@ def fanin_init(size, fanin=None):
 
 
 class CriticGNN(nn.Module):
-    def __init__(self, input_shape, edge_shape, action_shape, learning_rate, tau=0.001, init_w=3e-3):
+    def __init__(
+            self, input_shape, edge_shape,
+            action_shape, learning_rate, tau=0.001, init_w=3e-3
+    ):
         super(CriticGNN, self).__init__()
         self._learning_rate = learning_rate
         self._tau = tau
@@ -22,36 +25,35 @@ class CriticGNN(nn.Module):
         self.node_encoder = nn.Linear(input_shape, hidden_channels)
         self.edge_encoder = nn.Linear(edge_shape, hidden_channels)
 
-
         self.complex_gcn_in = GENConv(hidden_channels, 64, num_layers=4)
-        
+
         self.policy_layer_in = nn.Linear(64, 16)
         self.policy_layer_hidden = nn.Linear(16 + action_shape, 10)
         self.policy_layer_out = nn.Linear(10, 1)
 
         self.init_weights(init_w)
-                
+
     def init_weights(self, init_w):
         self.node_encoder.weight.data = fanin_init(self.node_encoder.weight.data.size())
         self.edge_encoder.weight.data = fanin_init(self.edge_encoder.weight.data.size())
-        
+
         self.policy_layer_in.weight.data = fanin_init(self.policy_layer_in.weight.data.size())
-        self.policy_layer_hidden.weight.data = fanin_init(self.policy_layer_hidden.weight.data.size())
+        self.policy_layer_hidden.weight.data = fanin_init(
+            self.policy_layer_hidden.weight.data.size()
+        )
         self.policy_layer_out.weight.data.uniform_(-init_w, init_w)
 
-    @timeit("critic_forward")
     def forward(self, state):
         complex_, action = state
         complex_data, complex_edge_index, \
             complex_edge_attr, complex_batch = complex_.x, complex_.edge_index,\
-                complex_.edge_attr, complex_.batch
+            complex_.edge_attr, complex_.batch
 
         complex_data = self.node_encoder(complex_data)
         complex_edge_attr = self.edge_encoder(complex_edge_attr)
 
-        
         complex_data = self.complex_gcn_in(complex_data, complex_edge_index, complex_edge_attr)
-    
+
         molecule_data = global_mean_pool(complex_data, complex_batch)
 
         finger_print = F.relu(self.policy_layer_in(molecule_data))
