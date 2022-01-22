3d350c6f5989f5e55d4f2272ee5da617ca1c4f3d
Justin Jose <justinj@thoughtworks.com>
2022-01-22 08:03:26 +0530

    [Justin] Introduce RL algorithm choice through config

--- training_script.py
+++ training_script.py
@@ -1,11 +1,24 @@
 from rlvs.molecule_world.env import GraphEnv
 from rlvs.agents.dqn_agent import DQNAgentGNN
+from rlvs.agents.ddpg_agent import DDPGAgentGNN
 from rlvs.agents.metrices import Metric
 from rlvs.config import Config
 
 import os
 import logging
 import numpy as np
+
+
+class TrainingAgents:
+    @staticmethod
+    def get_agent(algorithm: str):
+        agents = {
+            'dqn': DQNAgentGNN,
+            'ddpg': DDPGAgentGNN
+        }
+
+        return agents.get(algorithm)
+
 
 run_id = os.getenv('RUNID', 0)
 folder = os.getenv('OUTPUT', "./model/")
@@ -18,6 +31,8 @@ metric = Metric.get_instance()
 path_prefix = f"{folder}/run{run_id}_"
 log_filename = path_prefix+"training_log.log"
 
+TRAINING_AGENT = TrainingAgents.get_agent(config.training_algorithm)
+
 logging.basicConfig(
             filename=log_filename,
             filemode='w',
@@ -27,7 +42,7 @@ logging.basicConfig(
         )
 
 env = GraphEnv(single_step=np.array(config.single_step))
-agent = DQNAgentGNN(
+agent = TRAINING_AGENT(
     env,
     weights_path=path_prefix+"weights_intermediate",
     complex_path=path_prefix+"ligand_intermediate"
